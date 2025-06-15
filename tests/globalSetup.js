a2bdbfd21bf31fb2cd3476e7d2a532f9d13be6dd
Ultrainstictmasteryoda <128071340+dababiyoda@users.noreply.github.com>
2025-06-14 20:41:04 -0400

    fix(ci): wait for postgres

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -2,6 +2,15 @@ const { Pool } = require('pg');
 
 module.exports = async () => {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  await pool.query('CREATE EXTENSION IF NOT EXISTS pgcrypto');
+  for (let i = 0; i < 5; i += 1) {
+    try {
+      await pool.query('CREATE EXTENSION IF NOT EXISTS pgcrypto');
+      break;
+    } catch (err) {
+      if (i === 4) throw err;
+      console.log('Waiting for postgres...');
+      await new Promise((res) => setTimeout(res, 2000));
+    }
+  }
   await pool.end();
 };
