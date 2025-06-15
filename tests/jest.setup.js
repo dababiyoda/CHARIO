cdd3ea2a6df6b2b67d22887bcbea3e462ed417ad
Ultrainstictmasteryoda <128071340+dababiyoda@users.noreply.github.com>
2025-06-14 20:25:47 -0400

    ci: add postgres service for tests

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -3,7 +3,7 @@ nock('https://api.stripe.com').post(/.*/).reply(200, { success: true });
 
 process.env = {
   ...process.env,
-  DATABASE_URL: 'postgresql://chario:chario@localhost:5432/chario_test',
+  DATABASE_URL: 'postgresql://testuser:testpass@localhost:5432/testdb',
   JWT_SECRET: 'a'.repeat(32),
   STRIPE_KEY: 'sk_test_123',
   TWILIO_SID: 'ACxxx',
